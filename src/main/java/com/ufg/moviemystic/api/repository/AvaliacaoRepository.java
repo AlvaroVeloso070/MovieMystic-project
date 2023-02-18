ad8687a08fb6f6066f2b028ab3c1565aabd7d147
Álvaro Veloso <57870230+AlvaroVeloso070@users.noreply.github.com>
2023-02-18 00:49:40 -0300

    Revert "Crud funcionando, mas com listagens ainda mal otimizadas."
    This reverts commit d35b0f509e8c30033250191a317b1d10e586c7b4.

--- src/main/java/com/ufg/moviemystic/api/repository/AvaliacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ufg.moviemystic.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ufg.moviemystic.api.entities.Avaliacao;
-
-public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
-
-}
--- src/main/java/com/ufg/moviemystic/api/repository/CadastroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufg.moviemystic.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ufg.moviemystic.api.entities.Cadastro;
-
-public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
-}
